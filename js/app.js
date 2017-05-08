5f562c6139dee1fa71c829693cf5cc2cfcfee327
Marcelo Notebook <marcelojoc@gmail.com>
2017-05-08 00:27:34 -0300

    v-for de localidades funcionando

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var app = new Vue({
 
-
     el: 'body',
     data:{
-        nombre:'   mi nombre es thomas contreras'
-
+        nombre:'   mi nombre es thomas contreras',
+        localidad:['Capital','General Alvear',
+                    'Godoy Cruz','Guaymallén',
+                    'Junín','La Paz','Las Heras',
+                    'Lavalle' ,'Lujan de Cuyo',
+                    'Maipú','Malargue','Rivadavia',
+                    'San Carlos','San Martín',
+                    'San Rafael','Santa Rosa',
+                    'Tunuyán','Tupungato'
+        ]
     }
 
 })
