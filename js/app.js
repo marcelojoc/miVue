eaa7500e31fdd6aef9914fbdc62a13b26548b021
Marcelo tms <marcelojoc@gmail.com>
2017-05-15 12:23:30 -0300

    componente comp declarado funcional

--- js/app.js
+++ js/app.js
@@ -1,40 +1,88 @@
-var app = new Vue({
+// var app = new Vue({
 
-    el: 'body',
-    data:{
-        nombre:'   Bloque de Identificación del Territorio/Facilitador/Familia relevada',
-        localidad:['Capital','General Alvear',
-                    'Godoy Cruz','Guaymallén',
-                    'Junín','La Paz','Las Heras',
-                    'Lavalle' ,'Lujan de Cuyo',
-                    'Maipú','Malargue','Rivadavia',
-                    'San Carlos','San Martín',
-                    'San Rafael','Santa Rosa',
-                    'Tunuyán','Tupungato'
-        ]
-    }
+//     el: 'body',
+//     data:{
+//         nombre:'   Bloque de Identificación del Territorio/Facilitador/Familia relevada',
+//         localidad:['Capital','General Alvear',
+//                     'Godoy Cruz','Guaymallén',
+//                     'Junín','La Paz','Las Heras',
+//                     'Lavalle' ,'Lujan de Cuyo',
+//                     'Maipú','Malargue','Rivadavia',
+//                     'San Carlos','San Martín',
+//                     'San Rafael','Santa Rosa',
+//                     'Tunuyán','Tupungato'
+//         ]
+//     }
 
-});
+// });
 
 
 
 // todo crear componentes con vue
 Vue.component('comp1', 
     {
-        template:'<div>Component 1</div>',
+        template:'#combo',
+
+        ready:function(){
+
+           this.randomList();
+
+
+        },
 
         data:function(){
 
             return{
                 estado: false,
-                nombre: ""
+                nombre: "",
+                label: "",
+                list_label:['Capital','General Alvear',
+                            'Godoy Cruz','Guaymallén',
+                            'Junín','La Paz','Las Heras',
+                            'Lavalle' ,'Lujan de Cuyo',
+                            'Maipú','Malargue','Rivadavia',
+                            'San Carlos','San Martín',
+                            'San Rafael','Santa Rosa',
+                            'Tunuyán','Tupungato'
+                ]
             }
+        },
+
+
+
+
+
+
+        methods: {
+
+            mostrarDato: function(){
+
+
+                alert(this.estado);
+            },
+
+            randomList: function(){
+
+                // del array de departamentos saco uno aleatorio
+                var random =  this.list_label[Math.floor(Math.random() * this.list_label.length)];
+                // retorno el valor 
+                this.label=  random;
+
+            },
+
+            initComponent: function(){
+
+
+
+            }
+
+
+
+
+
+
+
         }
-
-
-
-
-
 
 
 
@@ -43,3 +91,10 @@ Vue.component('comp1',
 Vue.component('comp2', { 
   template:'<div>Component 2</div>' 
 }) 
+
+
+
+
+new Vue({
+  el: 'body'
+})
