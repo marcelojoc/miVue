c3de62b8407fd72604cbda8bd9a120f192fc86e3
Marcelo tms <marcelojoc@gmail.com>
2017-06-22 15:57:27 -0300

    indexedb DMozilla

--- js/indexedb.js
+++ js/indexedb.js
@@ -2,7 +2,7 @@
   
   
     if (!window.indexedDB) {
-    window.alert("Tu navegador esta obsoleto, so noporta caracteristicas que necesita esta aplicacion para funcionar");
+        window.alert("Tu navegador esta obsoleto, so noporta caracteristicas que necesita esta aplicacion para funcionar");
     }else{
 
         var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
@@ -30,6 +30,10 @@
         
     })
 
+
+
+
+
 })();
  
  
@@ -41,29 +45,44 @@
 
             dataBase.onsuccess = function (e) {
                 //alert('Base de datos cargada correctamente');
-                db = event.target.result
+                db = e.target.result
+                var objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+                
+                objectStore.createIndex("name", "name", { unique: false });
+
+                // Create an index to search customers by email. We want to ensure that
+                // no two customers have the same email, so use a unique index.
+                objectStore.createIndex("email", "email", { unique: true });
+
+                // Use transaction oncomplete to make sure the objectStore creation is 
+                // finished before adding data into it.
+                objectStore.transaction.oncomplete = function(event) {
+                    // Store values in the newly created objectStore.
+                    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+                    for (var i in customerData) {
+                    customerObjectStore.add(customerData[i]);
+                    }
+                }
+
+
             };
 
             dataBase.onerror = function (e)  {
-                alert('Error cargando la base de datos'+ event.target.message);
+                alert('Error cargando la base de datos'+ e.target.message);
             }; 
-        
-
-        
- 
-
+    
     }
 
 
 
 
-function guardar (dato){
-var objectStore = db.createObjectStore("bloque", { keyPath: "1" });
+    function guardar (dato){
+        var objectStore = db.createObjectStore("bloque", { keyPath: "1" });
 
-console.log(db)
+        console.log(db)
 
 
-}
+    }
 
 
 
